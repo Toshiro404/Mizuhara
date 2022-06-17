87f27cd5587c2756bddb29a7a8421304e9105abe
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-06-17 12:47:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.prefix= process.env.PREFIX || "-",
 global.author = process.env.AUTHOR || "ZEROTWO",
 global.sessionName = 'arus',
 global.name = process.env.NAME || "ZeroTwo",
-global.menu = "🍁 ZeroTwo\'s Command List* 🍁\n\n *━━━━❰ Dev ❱━━━━*\n❐ ```update, broadcast, ban, unban, chatid, session, join```\n\n *━━━━❰ General ❱━━━━*\n❐ ```help, delete, info, rank, profile```\n\n *━━━━❰ Fun ❱━━━━*\n❐ ```ship, advice, quote, leaderboard, react, fact, bot```\n\n *━━━━❰ Media ❱━━━━*\n❐ ```yta, ytv, play, gify, yts, lyrics```\n\n *━━━━❰ Moderation ❱━━━━*\n❐ ```demote, remove, ping, promote, deact, act, invite, group, seticon, setdesc```\n\n *━━━━❰ Weeb ❱━━━━*\n❐ ```anime, manga, charecter, haigusha, neko, waifu, pokemon```\n\n *━━━━❰ Utils ❱━━━━*\n❐ ```google, image, meme, subreddit, tourl, img, iguser, gif, sticker, take, pinterest, carbon```\n\n *🍁 powered by Arus 🍁*",
+global.owner = ['923087880256']
 global.thumb = fs.readFileSync('./src/info.jpeg')
 
 let file = require.resolve(__filename)
