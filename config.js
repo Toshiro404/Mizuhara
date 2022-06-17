812708f89ad818992cf75c803e0f1e8f38f40c10
Toshiro-sama <107214140+Toshiro404@users.noreply.github.com>
2022-06-17 16:01:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "none"
 global.port = process.env.PORT || 8080
 global.packname = process.env.PACKNAME || "by Arus",
-global.prefix= process.env.PREFIX || "-",
+global.prefix= process.env.PREFIX || "=",
 global.author = process.env.AUTHOR || "ZEROTWO",
 global.sessionName = 'arus',
 global.name = process.env.NAME || "ZeroTwo",
-global.owner = ['923087880256']
+global.owner = ['919609900020']
 global.thumb = fs.readFileSync('./src/info.jpeg')
 
 let file = require.resolve(__filename)
